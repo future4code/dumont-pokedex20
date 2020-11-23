7911b7f08f83f1125daf182c26764574f29b915c
crisrocha025 <crisfabricio0104@gmail.com>
2020-11-23 17:07:05 -0300

    Componente Header criado, Postamn Collection na pasta collectionPostman em src

--- src/screens/HomePage/HomePage.js
+++ src/screens/HomePage/HomePage.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import Header from '../../components/Header/Header'
+import {useHistory} from 'react-router-dom'
 
 function HomePage (){
+	const history = useHistory()
+	const openPokedex = () => {
+		history.push('/Pokedex')
+	}
     return(
         <div>
+        	<Header
+        		textbutton={'Minha Pokedex'}
+        		functionBtn={openPokedex}
+        	/>
             HomePage
         </div>
     )
